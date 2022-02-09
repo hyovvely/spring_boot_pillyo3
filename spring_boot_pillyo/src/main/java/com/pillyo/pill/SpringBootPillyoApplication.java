c4919ae965a7d7863e578685ed69c0fa1d60e034
HANGYESUK <akakakak66@naver.com>
2022-02-09 13:21:23 +0900

    first commit

---
+++ spring_boot_pillyo/src/main/java/com/pillyo/pill/SpringBootPillyoApplication.java
@@ -0,0 +1,13 @@
+package com.pillyo.pill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPillyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPillyoApplication.class, args);
+	}
+
+}
---
+++ spring_boot_pillyo/src/test/java/com/pillyo/pill/SpringBootPillyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pillyo.pill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPillyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
